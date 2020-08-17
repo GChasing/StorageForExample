143fad29a735b9d505d9e1065994e8b0ca3e288e
GChasing <541034902@qq.com>
2020-08-17 19:44:43 +0800

    add the rotation_S valide, it is not euqal to the rotation matrix

--- rotation_matrix_valid.m
+++ rotation_matrix_valid.m
@@ -7,3 +7,8 @@ rotation_s = [1     sin(phi)*tan(theta)     cos(phi)*tan(theta);
 rotation_s_T = rotation_s';
 rotation_s_T_inv = inv(rotation_s');
 rotation_s_inv_T = inv(rotation_s)';
+
+rotation = [1         0              -sin(theta);
+            0       cos(phi)       sin(phi)*cos(theta);
+            0        -sin(phi)      cos(phi)*cos(theta);];
+            
